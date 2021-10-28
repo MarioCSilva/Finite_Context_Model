36d3e941a195d3e3deb9239313c81a8fff7755ac
mario <mariosilva@ua.pt>
2021-10-28 12:00:38 +0100

    added hash table

--- src/fcm.py
+++ src/fcm.py
@@ -5,11 +5,16 @@ FILENAME = "./../example/example.txt"
 
 class FCM:
     def __init__(self, k, alpha, filename=FILENAME) -> None:
+        assert alpha > 0, "Values of alpha must be greater than zero"
+
         self.k = k
         self.alpha = alpha
         self.filename = filename
         self.symbol_dict = defaultdict(int)
-        self.prob_table = None
+        self.prob_table = []
+        self.is_hash_table = False
+        self.total_occurrences = 0
+
 
     def read_file(self):
         file_text = open(self.filename,"r")
@@ -32,7 +37,8 @@ class FCM:
         if self.has_memory_limit():
             self.prob_table = [[0] * self.alphabet_size for _ in range(self.alphabet_size** self.k)]
         else:
-            self.prob_table = {}
+            self.prob_table = defaultdict(lambda: defaultdict(int))
+            self.is_hash_table = True
 
         context = ""
 
@@ -45,28 +51,44 @@ class FCM:
                 if len(context) % (self.k+1) != 0:
                     continue
 
-                # add to occurencies table one more occurency of this char sequence
+                # add to occurrences table one more occurency of this char sequence
                 self.add_occur_to_table(context)
+                self.total_occurrences += 1
 
                 # clear first char of context
                 context = context[1:]
+
+        print(self.prob_table.keys())
 
 
     def has_memory_limit(self) -> bool:
         # verifies situations in which Memory Usage will be higher than 8GB
         mem = (self.alphabet_size ** self.k ) * self.alphabet_size * 16 /8/1024/1024
-        return mem >= 8,388,608
+        return mem >= 8388608
 
-    def char_probability(n_occorrences) -> None:
-        pass
+    def calc_probabilities(self) -> None:
+        if self.is_hash_table:
+            pass
+        else:
+            pass 
+
 
     def add_occur_to_table(self, context):
-        # pos_char_in_alphabet*len_alph**3 + pos_char_in_alphabet*len_alph**2 + pos_char_in_alphabet*len_alph**1
+        prefix = context[:-1]
+        next_char = context[-1]
+        
+        if self.is_hash_table:
+            self.prob_table[prefix][next_char] += 1
+            print( self.prob_table)
+            return
+
         context_index = 0
+        
         for i in range(self.k, 0, -1):
-            context_index += self.symbol_dict[context[i]] * self.alphabet_size**i
-        self.prob_table[context_index][self.symbol_dict[context[-1]] += 1
-    
+            context_index += self.symbol_dict[context[self.k-i]] * self.alphabet_size**(i-1)
+         
+        self.prob_table[context_index][self.symbol_dict[next_char]] += 1
+
     def calc_entropy(self):
         #Calculates the events probabilities in the FCM, the entropies for each context/row and the final entropy of the model.
         
@@ -102,5 +124,5 @@ class FCM:
         self.entropy = -self.entropy
 
 
-fcm = FCM(k=1,alpha=0)
+fcm = FCM(k=2,alpha=0.001)
 fcm.read_file()
