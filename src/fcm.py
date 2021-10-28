ad2f128a2d64b4fc8176c6d7f963a2b20847c742
mario <mariosilva@ua.pt>
2021-10-28 10:53:55 +0100

    added calc entropy

--- src/fcm.py
+++ src/fcm.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import math
 
 FILENAME = "./../example/example.txt"
 
@@ -8,7 +9,7 @@ class FCM:
         self.alpha = alpha
         self.filename = filename
         self.symbol_dict = defaultdict(int)
-        self.prob_table = []
+        self.prob_table = None
 
     def read_file(self):
         file_text = open(self.filename,"r")
@@ -19,7 +20,7 @@ class FCM:
             for char in line:
                 # non-aplhabetical count
                 # Lower upper case
-                if not self.symbol_dict.get(char):
+                if not self.symbol_dict[char]:
                     self.symbol_dict[char] = ind_counter
                     ind_counter += 1
 
@@ -29,7 +30,9 @@ class FCM:
 
         # memory formula
         if self.has_memory_limit():
-            self.prob_table = [0 for _ in range(self.alphabet_size**(self.k+1))]
+            self.prob_table = [[0] * self.alphabet_size for _ in range(self.alphabet_size** self.k)]
+        else:
+            self.prob_table = {}
 
         context = ""
 
@@ -59,9 +62,45 @@ class FCM:
 
     def add_occur_to_table(self, context):
         # pos_char_in_alphabet*len_alph**3 + pos_char_in_alphabet*len_alph**2 + pos_char_in_alphabet*len_alph**1
-        for i in range(self.k, -1, -1):
-            index = self.symbol_dict[context[i]] * self.alphabet_size**i
-            self.prob_table[index] += 1
+        context_index = 0
+        for i in range(self.k, 0, -1):
+            context_index += self.symbol_dict[context[i]] * self.alphabet_size**i
+        self.prob_table[context_index][self.symbol_dict[context[-1]] += 1
+    
+    def calc_entropy(self):
+        #Calculates the events probabilities in the FCM, the entropies for each context/row and the final entropy of the model.
+        
+        self.probs = self.dic_model # new dictionary with the same keys that dic_model, at the end this data structure will save the events probabilities
+
+        total = 0  
+        total_rows = {}
+        # for each context we calculate its total of occurences and we increment this to the total 
+        for context in self.dic_model.keys(): 
+            total_rows[context] = sum(self.dic_model[context].values()) 
+            total += total_rows[context]
+
+        for context in self.dic_model:
+            context_entropy = 0 
+            row_total = total_rows[context]
+
+            # calculate the events probabilities and context entropy
+            for char in self.dic_model[context]:
+                p_char = (self.dic_model[context][char] + self.alpha) / (row_total + self.alpha * self.cardinality)
+                self.probs[context][char] = p_char 
+                context_entropy += p_char * math.log2(p_char)
+            
+            # deal with zeros at lines, if alpha equals 0, zeros don't matter
+            if self.alpha > 0:
+                dif = self.cardinality - len(self.dic_model[context].values())
+                if dif != 0: # if dif differs to 0 this line have zeros
+                    p_char = self.alpha / (row_total + self.alpha * self.cardinality) # calculate the probability 
+                    context_entropy += dif * (p_char * math.log2(p_char)) # add the previoust value to context entropy n (dif) times
+
+            context_probability = row_total/total  # calculate context probability 
+            self.entropy += context_probability * context_entropy   # increase the final entropy
+
+        self.entropy = -self.entropy
+
 
 fcm = FCM(k=1,alpha=0)
 fcm.read_file()
